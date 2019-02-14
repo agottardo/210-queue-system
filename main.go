3cef61a09b6ea69494b45284fefde8aeeb25f2d4
Andrea Gottardo <andrea@gottardo.me>
2019-02-14 00:13:15 -0800

    First commit.

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -18,12 +21,62 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+	router.Delims("{{", "}}")
+	router.SetFuncMap(template.FuncMap{
+		"NumTimesHelped": NumTimesHelped,
+	})
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
-
+	router.GET("/status", handleStatus)
+	router.GET("/ta", handleTAStatus)
+	router.POST("/served", handleServed)
+	router.POST("/join", handleJoinReq)
 	router.Run(":" + port)
+}
+
+func handleJoinReq(c *gin.Context) {
+	name := c.PostForm("name")
+	CSid := c.PostForm("csid")
+	if !IsValidCSid(CSid) || name == "" {
+		hpv := HomePageValues{Error: "Invalid name or CS ID entered."}
+		c.HTML(http.StatusOK, "index.tmpl.html", hpv)
+		return
+	}
+	if HasJoinedQueue(CSid) {
+		hpv := HomePageValues{Error: "You have already joined the queue!"}
+		c.HTML(http.StatusOK, "index.tmpl.html", hpv)
+		return
+	}
+	aheadOfMe, waitTime := JoinQueue(name, CSid)
+	jpv := JoinedPageValues{
+		AheadOfMe:         strconv.Itoa(aheadOfMe),
+		HasEstimate:       waitTime != 0,
+		EstimatedWaitTime: strconv.Itoa(waitTime) + " seconds",
+		JoinedAt:          time.Now().String(),
+	}
+	c.HTML(http.StatusOK, "joined.tmpl.html", jpv)
+}
+
+func handleStatus(c *gin.Context) {
+	spv := StatusPageValues{UnservedEntries()}
+	c.HTML(http.StatusOK, "status.tmpl.html", spv)
+}
+
+func handleTAStatus(c *gin.Context) {
+	spv := StatusPageValues{UnservedEntries()}
+	c.HTML(http.StatusOK, "tastatus.tmpl.html", spv)
+}
+
+func handleServed(c *gin.Context) {
+	csid := c.PostForm("csid")
+	if csid == "" {
+		handleTAStatus(c)
+		return
+	}
+	ServeStudent(csid)
+	handleTAStatus(c)
 }
--- queue.go
+++ queue.go
@@ -1,1 +1,143 @@
 package main
+
+import (
+	"sync"
+	"time"
+)
+
+type QueueEntry struct {
+	CSid      string
+	Name      string
+	JoinedAt  time.Time
+	WasServed bool
+	ServedAt  time.Time
+}
+
+type Queue struct {
+	Mutex   sync.Mutex
+	Entries []QueueEntry
+}
+
+var queue = Queue{Entries: []QueueEntry{}}
+
+// Returns the estimated wait time in seconds.
+func EstimatedWaitTime() float64 {
+	var acc time.Duration
+	count := 0
+	queue.Mutex.Lock()
+	for _, entry := range queue.Entries {
+		if entry.WasServed {
+			waitTime := entry.ServedAt.Sub(entry.JoinedAt)
+			acc += waitTime
+			count++
+		}
+	}
+	queue.Mutex.Unlock()
+	if count == 0 {
+		// Nobody was served yet, no estimate available.
+		return 0
+	}
+	return acc.Seconds() / float64(count)
+}
+
+// Adds the student with name and CSid to the queue.
+// Returns how many students are ahead of the new student in the queue,
+// and the estimated wait time in seconds.
+func JoinQueue(name string, CSid string) (int, int) {
+	entry := QueueEntry{CSid, name, time.Now(), false, time.Now()}
+	queue.Mutex.Lock()
+	// How many un-served students joined before me?
+	rsf := 0
+	for _, entry := range queue.Entries {
+		if !entry.WasServed {
+			rsf++
+		}
+	}
+	queue.Entries = append(queue.Entries, entry)
+	queue.Mutex.Unlock()
+	return rsf, int(EstimatedWaitTime())
+}
+
+func HasJoinedQueue(CSid string) bool {
+	queue.Mutex.Lock()
+	for _, entry := range queue.Entries {
+		if entry.CSid == CSid && !entry.WasServed {
+			queue.Mutex.Unlock()
+			return true
+		}
+	}
+	queue.Mutex.Unlock()
+	return false
+}
+
+func ServeStudent(CSid string) {
+	queue.Mutex.Lock()
+	for i, entry := range queue.Entries {
+		if entry.CSid == CSid {
+			if !entry.WasServed {
+				queue.Entries[i].ServedAt = time.Now()
+			}
+			queue.Entries[i].WasServed = true
+		}
+	}
+	queue.Mutex.Unlock()
+}
+
+func UnservedEntries() []QueueEntry {
+	acc := []QueueEntry{}
+	queue.Mutex.Lock()
+	for _, entry := range queue.Entries {
+		if !entry.WasServed {
+			acc = append(acc, entry)
+		}
+	}
+	queue.Mutex.Unlock()
+	return acc
+}
+
+func TAEntries() []QueueEntry {
+	acc := []QueueEntry{}
+	queue.Mutex.Lock()
+	for _, entry := range queue.Entries {
+		if !entry.WasServed {
+			acc = append(acc, entry)
+		}
+	}
+	queue.Mutex.Unlock()
+	return acc
+}
+
+func NumTimesHelped(CSid string) int {
+	acc := 0
+	queue.Mutex.Lock()
+	for _, entry := range queue.Entries {
+		if entry.CSid == CSid {
+			acc++
+		}
+	}
+	queue.Mutex.Unlock()
+	return acc
+}
+
+// TODO: actual pattern matching letter-number-letter-number...
+func IsValidCSid(id string) bool {
+	if len(id) != 4 && len(id) != 5 {
+		return false
+	}
+	return true
+}
+
+type HomePageValues struct {
+	Error string
+}
+
+type JoinedPageValues struct {
+	AheadOfMe         string
+	HasEstimate       bool
+	EstimatedWaitTime string
+	JoinedAt          string
+}
+
+type StatusPageValues struct {
+	Entries []QueueEntry
+}
